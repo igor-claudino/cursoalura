13ccfd8aafb9f9bd74ac1cb8ae798e3a40505560
Igor Claudino de França Costa <igor.claudino.ic@gmail.com>
2018-10-11 16:22:25 -0300

    Finalizacao da Aula 5

---
+++ aluraframe/client/js/app/views/NegociacoesView.js
@@ -0,0 +1,52 @@
+class NegociacoesView {
+
+    constructor(elemento) {
+        this._elemento = elemento;
+    }
+
+    update(model) {
+        this._elemento.innerHTML = this._template(model);
+    }
+
+    _template(model) {
+        return `
+        <table class="table table-hover table-bordered">
+        <thead>
+            <tr>
+                <th>DATA</th>
+                <th>QUANTIDADE</th>
+                <th>VALOR</th>
+                <th>VOLUME</th>
+            </tr>
+        </thead>
+    
+        <tbody>
+            ${model.negociacoes.map(n => `
+                    
+                    <tr>
+                        <td>${DateHelper.dataParaTexto(n.data)}</td>
+                        <td>${n.quantidade}</td>
+                        <td>${n.valor}</td>
+                        <td>${n.volume}</td>
+                    </tr>
+
+            `).join('')}
+        </tbody>
+    
+        <tfoot>
+            <td colSpan="3"></td>
+            <td>${
+            model.negociacoes.reduce((total, n) => total + n.volume, 0.0)
+            // (function () {
+            //     let total = 0;
+            //     model.negociacoes.forEach(n =>
+            //         total += n.volume
+            //     );
+            //     return total;
+            // })()
+            }</td>
+        </tfoot>
+        </table>`;
+    }
+
+}
